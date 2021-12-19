1777d9e65b1efbac76fa05627c0548498ebd7fdb
chihab <chihab.mg.me@gmail.com>
2021-12-19 12:31:17 +0100

    adding mobile menu

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ let show_btn = document.getElementById('menu-show')
 let menu = document.getElementById('menu')
 let show = false 
 window.addEventListener('resize',()=>{
+    if(!show){
     menu.classList.remove('show')
     show = false
+}
 })
 show_btn.addEventListener('click',()=>{
     if(!show){
